4cc50190dae4fae05e2f3da66d9bce54749a17b7
nabilasusan <nabila_2226240101@mhs.mdp.ac.id>
2024-09-20 10:39:49 +0700

    nodejs

---
+++ index.js
@@ -0,0 +1,20 @@
+// inisialisasi modul http
+const http = require('htpp');
+
+// buat server
+const server = http.createServer((req, res)=>
+{
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write('Selamat pagi');
+    res.end();
+} );
+
+const port = 3000;
+const host ='localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan di http://${host}:${port}`);
+} );
+
+
+
